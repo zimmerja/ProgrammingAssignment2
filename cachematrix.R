d1ee11740bdfef0ab0c8431fc8f66f3e300afd0b
zimmerja <jeffacctcl@gmail.com>
2015-03-22 08:04:05 -0700

    R-Programming Coursera Assignment 2
    Code to create inverse of matrix by pulling from cache or calculating it

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## OVERALL, THESE TWO FUNCTIONS FIRST CREATES
+## A LIST TO GET AND STORE A MATRIX, AND GET AND
+## STORE THE INVESRSE OF A MATRIX.  THE SECOND FUNCTION
+## WILL RETURN THE INVERSE OF A MATRIX, EITHER RETRIEVING
+## IT FROM THE CACHE, OR CALCULATING THE INVERSE
 
-## Write a short comment describing this function
+## THIS FUNCTION CREATES A LIST OF THE FUNCTIONS
+## TO GET AND STORE THE INVERSE OF A MATRIX
+## WITHIN A CHILD ENVIRONMENT
 
 makeCacheMatrix <- function(x = matrix()) {
-
+   inv <- NULL
+   set <- function(y) {
+      x <<- y
+      inv <- NULL
+   }
+   get <- function() x
+   setinv <- function(myinv) inv <<- myinv
+   getinv <- function() inv
+   list(set = set, get=get,
+        setinv = setinv,
+        getinv = getinv
+      )
 }
 
-
-## Write a short comment describing this function
+## THIS FUNCTION WILL COMPUTE THE INVERSE
+## BY EITHER RETRIEVING IT FRM THE CACHE
+## IF IT ALREADY EXISTS, OR CALCULATING IT
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   inv <- x$getinv()
+   if(!is.null(inv)) {
+      message("getting cached inverse")
+      return(inv)
+   }
+   data <- x$get()
+   inv <- solve(x)
+   x$setinv(inv)
+   inv
 }
